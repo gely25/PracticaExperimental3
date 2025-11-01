e1d2be55fa014f04961019c884a2cc2d9f115834
gely25 <angesegoviachavez@gmial.com>
2025-11-01 15:36:25 -0500

    Modificación para Conteo de Personas

--- vision/urls.py
+++ vision/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
 from . import views
 
+
+
 urlpatterns = [
     path('', views.index_view, name='index'),
     path('video_feed/', views.video_feed, name='video_feed'),
-    path('mark/', views.mark_attendance, name='mark_attendance'),
     path('toggle_camera/', views.toggle_camera, name='toggle_camera'),
     path('stats/', views.stats, name='stats'),
-    path('report/', views.report_view, name='report'),  
-
-
 ]
+
+
+
 
 
